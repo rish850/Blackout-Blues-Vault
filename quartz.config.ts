ac9c3a12a1c1fd89adfd1eec2cfaef3ce471d16d
Rish <rish.samonina@gmail.com>
2024-12-22 18:12:24 +0200

    Quartz sync: Dec 22, 2024, 6:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🐦‍⬛ Blackout Blues",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rish850.github.io/Blackout-Blues-Vault",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
